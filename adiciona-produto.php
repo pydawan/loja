602e174eaa8c6ae9dd3a8fd2fb0f01ae31ea385e
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2017-04-06 10:51:09 -0400

    add mysql query to add produtos

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -2,6 +2,20 @@
     <?php
         $nome = $_GET["nome"];
         $preco = $_GET["preco"];
+        $conexao = mysqli_connect('127.0.0.1', 'root', '123456', 'loja');
+        
+        $query = "insert into produtos(nome, preco) values ('{$nome}', {$preco})";
+        if (mysqli_query($conexao, $query)) {
+            ?>
+                <p class="alert-success">Produto <?= $nome ?> no valor de <?= $preco ?> adicionado com sucesso!</p>
+            <?php
+        } else { ?>
+            <p class="alert-danger">Produto <?= $nome ?> nao adicionado com sucesso!</p>
+            <?php
+        }
+        
+        mysqli_close($conexao);
+        
     ?>
-    <p class="alert-success">Produto <?= $nome ?> no valor de <?= $preco ?> adicionado com sucesso!</p>
+    
 <?php include("rodape.php")?>
