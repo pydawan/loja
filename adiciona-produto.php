44033d85ce6d897a10e50e73a319320b69f726b8
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2017-04-07 14:13:37 -0400

    add bootstrap style and fix mysql connection

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,21 +1,23 @@
 <?php include("cabecalho.php")?>
     <?php
+        function insereProduto($conexao, $nome, $preco){
+            $query = "insert into produtos(nome, preco) values ('{$nome}', {$preco})";
+            return mysqli_query($conexao, $query);
+        }
         $nome = $_GET["nome"];
         $preco = $_GET["preco"];
         $conexao = mysqli_connect('127.0.0.1', 'root', '123456', 'loja');
         
-        $query = "insert into produtos(nome, preco) values ('{$nome}', {$preco})";
-        if (mysqli_query($conexao, $query)) {
+        if (insereProduto($conexao, $nome, $preco)) {
             ?>
-                <p class="alert-success">Produto <?= $nome ?> no valor de <?= $preco ?> adicionado com sucesso!</p>
+                <p class="text-success">Produto <?= $nome ?> no valor de <?= $preco ?> adicionado com sucesso!</p>
             <?php
-        } else { ?>
-            <p class="alert-danger">Produto <?= $nome ?> nao adicionado com sucesso!</p>
+        } else {
+            $msg = mysqli_error($conexao);
+            ?>
+            <p class="text-danger">Produto <?= $nome ?> nao adicionado: <?= $msg ?></p>
             <?php
         }
-        
-        mysqli_close($conexao);
-        
     ?>
     
 <?php include("rodape.php")?>
--- produto-formulario.php
+++ produto-formulario.php
@@ -1,11 +1,29 @@
 <?php include("cabecalho.php")?>
     <h1> Formulario de produto</h1>
     <form action="adiciona-produto.php">
-        Nome:
-        <input type="text" name="nome"><br />
-        Preco:
-        <input type="number" name="preco"><br />
-                    
-        <input type="submit" value= "Cadastrar">
+        <table class="table">
+            <tr>
+                <td>
+                    Nome:
+                </td>
+                <td>
+                    <input type="text" name="nome" class="form-control"><br />  
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Preco:
+                </td>
+                <td>
+                    <input type="number" name="preco" class="form-control"><br />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" value= "Cadastrar" class="btn btn-primary"> 
+                </td>
+            </tr>
+        </table>
+        
     </form>
 <?php include("rodape.php")?>
